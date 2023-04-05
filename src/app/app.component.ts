84e57f25eb2715566556db15be4e2e6ad4e07d1b
Sasha <sashla1713@gmail.com>
2023-04-05 13:34:35 +0300

    Added http client module and error component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
-import { products as data } from './data/products';
+import { ProductsService } from './services/products.service';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'my app';
 
-  products: IProduct[] = data
+  //products: IProduct[] = []
+  products$: Observable<IProduct[]>
+  loading = false
+
+  constructor(private productsService: ProductsService){
+
+  }
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(()=>this.loading = false)
+    )
+    // this.productsService.getAll().subscribe(products =>{
+    //   this.products = products
+    //   this.loading = false
+    // })
+  }
 }
